17c596dd250f1e6b95d3c5184fdc77ee7862d1d5
moris96 <moriskhoudari@gmail.com>
2023-03-28 13:06:17 -0400

    Py: maths: simple array sum

--- Py/algos.py
+++ Py/algos.py
@@ -5,3 +5,13 @@ import random
 import re
 import sys
 
+
+#simple array sum 
+# def simpleArraySum(ar):
+#     n = 0 
+#     for i in ar:
+#         n = n + i 
+#     return n 
+# print(simpleArraySum([1,2,3]))
+
+
