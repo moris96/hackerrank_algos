05bbd34d490d51ef9fc103a286477a4267581af8
moris96 <moriskhoudari@gmail.com>
2023-04-11 15:18:20 -0400

    JS: strings: camel case word count - super fun challenge actually lol

---
+++ JS/algos.js
@@ -0,0 +1,13 @@
+//camel case
+// function camelcase(s) {
+//     let numWords = 0
+//     for(const i in s){
+//         const c = s.charAt(i)
+//         if(c>='A' && c<='Z'){
+//             numWords++ 
+//         }
+//     }
+//     numWords = numWords+1
+//     return numWords
+// }
+// console.log(camelcase('saveChangesInTheEditor')) 
