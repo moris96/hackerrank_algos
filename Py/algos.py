dc492f4d592093d8e12189425432d2eb28ab42df
moris96 <moriskhoudari@gmail.com>
2023-08-08 12:50:27 -0400

    Py: maths: bill division

--- Py/algos.py
+++ Py/algos.py
@@ -121,3 +121,14 @@ import sys
 # def gradingStudents(grades):
 #     return [i if i<38 else i + adds[i%5] for i in grades]
 
+
+#bill division
+# def bonAppetit(bill, k, b):
+#     count = 0
+#     for i in range(len(bill)):
+#         if (i != k): count += bill[i]
+#     pay = count//2
+#     if(pay != b): print(b - pay)
+#     else: print("Bon Appetit")
+
+
